ef4178d4aad1eaf260a60f62526c54a5055e577c
Joshua Tucker <jtucker@elevenfifty.org>
2023-05-31 11:32:27 -0400

    UserEntity and DbSet added

---
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Data.Entities;
+
+public class UserEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+    [MaxLength(100)]
+    public string? FirstName { get; set; }
+
+    [MaxLength(100)]
+    public string? LastName { get; set; }
+
+    [Required]
+    public DateTime DateCreated { get; set; }
+}
