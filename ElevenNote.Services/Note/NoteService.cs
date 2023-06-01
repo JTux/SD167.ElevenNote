15597d63d4106ba6026127ae9f3455355a7d4885
Joshua Tucker <jtucker@elevenfifty.org>
2023-06-01 16:06:30 -0400

    Create Note Functionality

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using ElevenNote.Data;
+using ElevenNote.Data.Entities;
 using ElevenNote.Models.Note;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,33 @@ public class NoteService : INoteService
             throw new Exception("Attempted to build NoteService without User Id claim.");
 
         _dbContext = dbContext;
+    }
+
+    public async Task<NoteListItem?> CreateNoteAsync(NoteCreate request)
+    {
+        var noteEntity = new NoteEntity
+        {
+            Title = request.Title,
+            Content = request.Content,
+            CreatedUtc = DateTimeOffset.Now,
+            OwnerId = _userId
+        };
+
+        _dbContext.Notes.Add(noteEntity);
+        var numberOfChanges = await _dbContext.SaveChangesAsync();
+
+        if(numberOfChanges == 1)
+        {
+            NoteListItem response = new()
+            {
+                Id = noteEntity.Id,
+                Title = noteEntity.Title,
+                CreatedUtc = noteEntity.CreatedUtc
+            };
+            return response;
+        }
+
+        return null;
     }
 
     public async Task<IEnumerable<NoteListItem>> GetAllNotesAsync()
--- ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -1,3 +1,4 @@
+using ElevenNote.Models.Note;
 using ElevenNote.Services.Note;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,20 @@ namespace ElevenNote.WebApi.Controllers
         public NoteController(INoteService noteService)
         {
             _noteService = noteService;
+        }
+
+        // POST api/Note
+        [HttpPost]
+        public async Task<IActionResult> CreateNote([FromBody] NoteCreate request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var response = await _noteService.CreateNoteAsync(request);
+            if (request is not null)
+                return Ok(request);
+
+            return BadRequest("Could not create Note.");
         }
 
         // GET api/Note
