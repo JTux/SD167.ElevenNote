0e5c97a7547f488b47b39a130e955b4d408a5277
Joshua Tucker <jtucker@elevenfifty.org>
2023-07-24 15:46:22 -0400

    Cleaned up NoteService comments and removed Service package

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -1,8 +1,6 @@
-using System.Security.Claims;
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.Note;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,8 +15,8 @@ public class NoteService : INoteService
         UserManager<UserEntity> userManager,
         SignInManager<UserEntity> signInManager)
     {
-        var claim = userManager.GetUserId(signInManager.Context.User);
-        var validId = int.TryParse(claim, out _userId);
+        var userIdClaim = userManager.GetUserId(signInManager.Context.User);
+        var validId = int.TryParse(userIdClaim, out _userId);
         if (!validId)
             throw new Exception("Attempted to build NoteService without User Id claim.");
 
@@ -27,33 +25,46 @@ public class NoteService : INoteService
 
     public async Task<NoteListItem?> CreateNoteAsync(NoteCreate request)
     {
-        var noteEntity = new NoteEntity
+        // Create a new note entity based on the content from the request model
+        // Also set the CreatedUtc based on the current time, and the OwnerId by the _userId field
+        NoteEntity noteEntity = new()
         {
             Title = request.Title,
             Content = request.Content,
             CreatedUtc = DateTimeOffset.Now,
             OwnerId = _userId
         };
-
+        
+        // Add the new entity to our Notes DbSet, prepping it to be saved to the database
         _dbContext.Notes.Add(noteEntity);
+
+        // Attempt to close out the transaction, saving the number of rows modified in the database
         var numberOfChanges = await _dbContext.SaveChangesAsync();
 
+        // If the number of changes is equal to 1, then we know the note was saved
         if (numberOfChanges == 1)
         {
+            // After saved, create our response model with the entity information
+            // This includes the new Id, assigned when the entity was saved
             NoteListItem response = new()
             {
                 Id = noteEntity.Id,
                 Title = noteEntity.Title,
                 CreatedUtc = noteEntity.CreatedUtc
             };
+            // Return the response model
             return response;
         }
 
+        // If the number of changes saved is not equal to 1, return null instead of an object
         return null;
     }
 
     public async Task<IEnumerable<NoteListItem>> GetAllNotesAsync()
     {
+        // Query the _dbContext and Notes DbSet
+        // The .Where filters all Notes that match the current _userId value
+        // Using Select, convert our Entities to our NoteListItem model type
         var notes = await _dbContext.Notes
             .Where(entity => entity.OwnerId == _userId)
             .Select(entity => new NoteListItem
